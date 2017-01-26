8cc317bade29fc6fabc8210b50c200c882c91ae3
matsrietdijk <matsrietdijk@gmail.com>
2017-01-26 21:40:35 +0100

    Add single file components code

---
+++ single-file-components/src/components/Hello.vue
@@ -0,0 +1,26 @@
+<template>
+    <div>
+        <p>Hello component!</p>
+        <p @click="increment">
+            You clicked this element {{ counter }} {{ counter | pluralize('time', 'times') }}
+        </p>
+    </div>
+</template>
+
+<script>
+export default {
+    data () {
+        return { counter: 0 };
+    },
+    methods: {
+        increment () {
+            this.counter += 1;
+        }
+    },
+    filters: {
+        pluralize (count, single, multiple) {
+            return count === 1 ? single : multiple;
+        }
+    }
+};
+</script>
